943406bcd83154b2a0b3428065efe7e7b6830764
shofiyyah 2sier <shofiyyah24si@mahasiswa.pcr.ac.id>
2025-09-24 09:28:28 +0700

    Penambahan HomeController

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,7 @@ Route::get('/nama/{param1?}/{nim?}', function ($param1='',$nim='') {
 Route::get('/about', function () {
     return view('halaman-about');
 });
+
+use App\Http\Controllers\HomeController;
+Route::get('/home',[HomeController::class, 'index']);
+
